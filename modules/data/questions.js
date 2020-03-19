694db8cde3564d1f7e153d069f5ba65cc7c8f790
MarjoleinAardewijn <marjolein.aardewijn@gmail.com>
2020-03-19 20:24:14 +0100

    basic forms (HTML)

---
+++ modules/data/questions.js
@@ -0,0 +1,104 @@
+const questions = [
+    {
+        id: 1,
+        name: "q1",
+        question: "Vraag 1",
+        options: {
+            a: "Antwoord 1",
+            b: "Antwoord 2",
+            c: "Antwoord 3"
+        }
+    },
+    {
+        id: 2,
+        name: "q2",
+        question: "Vraag 2",
+        options: {
+            a: "Antwoord 1",
+            b: "Antwoord 2",
+            c: "Antwoord 3"
+        }
+    },
+    {
+        id: 3,
+        name: "q3",
+        question: "Vraag 3",
+        options: {
+            a: "Antwoord 1",
+            b: "Antwoord 2",
+            c: "Antwoord 3"
+        }
+    },
+    {
+        id: 4,
+        name: "q4",
+        question: "Vraag 4",
+        options: {
+            a: "Antwoord 1",
+            b: "Antwoord 2",
+            c: "Antwoord 3"
+        }
+    },
+    {
+        id: 5,
+        name: "q5",
+        question: "Vraag 5",
+        options: {
+            a: "Antwoord 1",
+            b: "Antwoord 2",
+            c: "Antwoord 3"
+        }
+    },
+    {
+        id: 6,
+        name: "q6",
+        question: "Vraag 6",
+        options: {
+            a: "Antwoord 1",
+            b: "Antwoord 2",
+            c: "Antwoord 3"
+        }
+    },
+    {
+        id: 7,
+        name: "q7",
+        question: "Vraag 7",
+        options: {
+            a: "Antwoord 1",
+            b: "Antwoord 2",
+            c: "Antwoord 3"
+        }
+    },
+    {
+        id: 8,
+        name: "q8",
+        question: "Vraag 8",
+        options: {
+            a: "Antwoord 1",
+            b: "Antwoord 2",
+            c: "Antwoord 3"
+        }
+    },
+    {
+        id: 9,
+        name: "q9",
+        question: "Vraag 9",
+        options: {
+            a: "Antwoord 1",
+            b: "Antwoord 2",
+            c: "Antwoord 3"
+        }
+    },
+    {
+        id: 10,
+        name: "q10",
+        question: "Vraag 10",
+        options: {
+            a: "Antwoord 1",
+            b: "Antwoord 2",
+            c: "Antwoord 3"
+        }
+    }
+];
+
+module.exports = { questions };
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-// Require (third-party) modules
 const express = require('express'),
     globalConfig = require('./config'),
+    fs = require("./modules/helpers/fs"),
+    urlStorage = require("./modules/helpers/url-storage"),
+    answersFile = require("./answers"),
+    questions = require('./modules/data/questions'),
+    questionsData = questions.questions,
     app = express(),
     config = {
         port: globalConfig.port
@@ -14,8 +18,28 @@ app
     .use(express.static('static'))
 
     .get('/', function(req, res) {
+        let queries = urlStorage.setUrlStorageQuestionsForm(req.query);
+
         res.render('home', {
-            title: 'Minor Web Development'
+            title: 'Minor Web Development',
+            queries
+        })
+    })
+    .get('/questions', function(req, res) {
+        let queries = urlStorage.setUrlStorageHomeForm(req.query);
+
+        res.render('questions', {
+            title: 'Questions',
+            questionsData,
+            queries
+        })
+    })
+
+    .get('/thanks', function(req, res) {
+        fs.writeAnswersToJsonFile(answersFile, req.query);
+
+        res.render('thanks', {
+            title: 'Thanks!'
         })
     })
 
